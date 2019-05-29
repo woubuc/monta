896764396c0a501036e0a3dfd51e5d438f6ed170
woubuc <wouter@epicteddy.com>
2019-05-29 12:52:54 +0200

    Replaced coveralls with codecov

--- jest.config.base.js
+++ jest.config.base.js
@@ -1,9 +1,15 @@
 module.exports = {
 	preset: 'ts-jest',
+
+	verbose: true,
+
 	testEnvironment: 'node',
 	testMatch: ['**/packages/*/tests/**/*.ts'],
 
+	coverageDirectory: './coverage/',
+
 	moduleNameMapper: {
+		'^monta$': '<rootDir>/../monta/src',
 		'^(monta(-.*)?)$': '<rootDir>/../$1/src/',
 	},
 };
